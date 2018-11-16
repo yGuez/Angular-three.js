25c13b79ef23c0cd941ee7ef358aa7e60d5431f0
GUEZENNEC Yves <YGUEZENNEC@tibco.fr>
2018-11-16 17:30:57 +0100

    ajout libs

---
+++ src/app/ui/ui-infobar-top/ui-infobar-top.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-infobar-top',
+  templateUrl: './ui-infobar-top.component.html',
+  styleUrls: ['ui-infobar-top.css']
+})
+export class UiInfobarTopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
